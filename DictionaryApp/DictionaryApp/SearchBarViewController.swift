22bf21e9df88d03fe2ae1f5429d81df7a889d9be
bkalchv <bkaltchev98@gmail.com>
2022-02-09 12:44:51 +0200

    moves searching/querying logic from SearchBarVC to SearchEngine, adds a property of type SearchEngine in SearchBarViewController, improves input validation in SearchEngine (lower-case first letter fix, non-letter characters not accepted))

--- DictionaryApp/DictionaryApp/SearchBarViewController.swift
+++ DictionaryApp/DictionaryApp/SearchBarViewController.swift
@@ -9,10 +9,9 @@ import UIKit
 
 class SearchBarViewController: UIViewController, UISearchBarDelegate {
 
-    var wordsDictionary: [String : [DictionaryToken]] = [String : [DictionaryToken]]()
+    var searchEngine: SearchEngine = SearchEngine()
     
     @IBOutlet weak var searchBar: UISearchBar!
-    @IBOutlet weak var DecodeAndPrintA: UIButton!
     @IBOutlet weak var wordLabel: UILabel!
     @IBOutlet weak var translationTextField: UITextView!
     
@@ -22,111 +21,19 @@ class SearchBarViewController: UIViewController, UISearchBarDelegate {
         searchBar.delegate = self
         wordLabel.text = ""
         translationTextField.text = ""
-        translationTextField.isEditable = false
-        translationTextField.isSelectable = false
-        DecodeAndPrintA.isHidden = true
-    }
-    
-    func decodeFileForLetter(letter: String) -> [DictionaryToken] {
-        
-        var result = [DictionaryToken]()
-        
-        let fileManager = FileManager.default
-        let urls = fileManager.urls(for: .cachesDirectory, in: .userDomainMask)
-        let cachesDirectoryUrl = urls[0]
-        
-        let fileUrl = cachesDirectoryUrl.appendingPathComponent(letter)
-        //print(fileUrl.absoluteString)
-        
-        let filePath = fileUrl.path
-        
-        if fileManager.fileExists(atPath: filePath) {
-            do {
-                let data = try Data(contentsOf: fileUrl)
-                let decoder = JSONDecoder()
-                result = try decoder.decode([DictionaryToken].self, from: data)
-                print("DictionaryTokens at \(filePath) read.")
-                //print(dictionaryTokens)
-            } catch {
-                print(error)
-            }
-        } else {
-            print("File \(filePath) doesn't exist")
-        }
-        
-        return result
-    }
-    
-    // TODO: redundant (had testing purposes)
-    @IBAction func onDecodeAndPrintAButtonClick(_ sender: Any) {
-        let tokens: [DictionaryToken] = decodeFileForLetter(letter: "A")
-        print(tokens)
-    }
-    
-    func firstLetterAsString(ofInput input: String) -> String {
-        var result = ""
-        
-        if let firstChar = input.first?.description {
-            result = firstChar
-        }
-        
-        return result
-    }
-    
-    func doesKeyExistInWordsDictionary(withKey key: String) -> Bool {
-        return wordsDictionary.keys.contains(key)
-    }
-    
-    func containsWordsWithPrefix(withDictionaryTokenArray dictionaryTokensArray: [DictionaryToken], withPrefix prefix: String) -> Bool {
-        let dictionaryTokensWithPrefix = dictionaryTokensArray.filter( {return $0.word.hasPrefix(prefix)} )
-        return !dictionaryTokensWithPrefix.isEmpty
-    }
-    
-    func findLongestPrefixInWordsDictionary(ofWord word: String) -> String? {
-        
-        let firstLetterOfWord = firstLetterAsString(ofInput: word)
-        
-        if firstLetterOfWord != "", let words = wordsDictionary[firstLetterOfWord] {
-            var endIndex = word.endIndex
-            var currentRangeOfInterest = word.startIndex..<endIndex
-            var currentPrefix = String(word[currentRangeOfInterest])
-            
-            while !containsWordsWithPrefix(withDictionaryTokenArray: words, withPrefix: currentPrefix) && endIndex != word.startIndex {
-                endIndex = word.index(before: endIndex)
-                currentRangeOfInterest = word.startIndex..<endIndex
-                currentPrefix = String(word[currentRangeOfInterest])
-            }
-            
-            if (endIndex == word.startIndex) { return nil }
-            
-            return currentPrefix
-            
-        } else { return nil }
-
-    }
-    
-    func findClosestMatchInWordsDictionary(toInput input: String) -> DictionaryToken? {
-        let firstLetterOfInput = firstLetterAsString(ofInput: input)
-        if  firstLetterOfInput != "",
-            let words = wordsDictionary[firstLetterOfInput],
-            let longestPrefix = findLongestPrefixInWordsDictionary(ofWord: input) {
-            
-                let dictionaryTokensWithCurrentPrefix = words.filter( { return $0.word.hasPrefix(longestPrefix)} )
-               
-                if let first = dictionaryTokensWithCurrentPrefix.first { return first }
-            }
-        
-        return nil
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        if !searchText.isEmpty {
-            let firstLetterOfSearchText = firstLetterAsString(ofInput: searchText)
-            if !doesKeyExistInWordsDictionary(withKey: firstLetterOfSearchText) {
-               wordsDictionary[firstLetterOfSearchText] = decodeFileForLetter(letter: firstLetterOfSearchText) // loads words in wordsDictionary
+        if !searchText.isEmpty, let firstLetterOfSearchText = searchText.first, firstLetterOfSearchText.isLetter {
+            // TODO: handle pasting
+            // TODO: handle first letter not being capital letter
+            // TODO: handle first not being a letter
+            let firstLetterOfSearchTextAsUppercasedString = firstLetterOfSearchText.uppercased()
+            if !searchEngine.doesKeyExistInWordsDictionary(withKey: firstLetterOfSearchTextAsUppercasedString) {
+                searchEngine.wordsDictionary[firstLetterOfSearchTextAsUppercasedString] = searchEngine.decodeFileForLetter(letter: firstLetterOfSearchTextAsUppercasedString) // loads words in wordsDictionary
             }
             
-            if let closestMatch: DictionaryToken = findClosestMatchInWordsDictionary(toInput: searchText.uppercased()) {
+            if let closestMatch: DictionaryToken = searchEngine.findClosestMatchInWordsDictionary(toInput: searchText.uppercased()) {
                 //print(closestMatch)
                 wordLabel.text = closestMatch.word
                 translationTextField.text = closestMatch.translation
@@ -142,7 +49,7 @@ class SearchBarViewController: UIViewController, UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         if let searchBarText = searchBar.text {
             
-            if let closestMatch: DictionaryToken = findClosestMatchInWordsDictionary(toInput: searchBarText.uppercased()) {
+            if let closestMatch: DictionaryToken = searchEngine.findClosestMatchInWordsDictionary(toInput: searchBarText.uppercased()) {
                 print(closestMatch)
                 wordLabel.text = closestMatch.word
                 translationTextField.text = closestMatch.translation
---
+++ DictionaryApp/DictionaryApp/SearchEngine.swift
@@ -0,0 +1,101 @@
+//
+//  SearchEngine.swift
+//  DictionaryApp
+//
+//  Created by Bogdan Kalchev on 8.02.22.
+//
+
+import Foundation
+
+class SearchEngine {
+    
+    var wordsDictionary: [String : [DictionaryToken]] = [String : [DictionaryToken]]()
+    
+    func decodeFileForLetter(letter: String) -> [DictionaryToken] {
+        
+        // TODO: ask - should there be a check for the letter itself
+        // or can we rely on the fact we call the function once we are sure that the
+        // argument is a letter?
+        
+        var result = [DictionaryToken]()
+
+        let fileManager = FileManager.default
+        let urls = fileManager.urls(for: .cachesDirectory, in: .userDomainMask)
+        let cachesDirectoryUrl = urls[0]
+
+        let fileUrl = cachesDirectoryUrl.appendingPathComponent(letter)
+        //print(fileUrl.absoluteString)
+
+        let filePath = fileUrl.path
+
+        if fileManager.fileExists(atPath: filePath) {
+            do {
+                let data = try Data(contentsOf: fileUrl)
+                let decoder = JSONDecoder()
+                result = try decoder.decode([DictionaryToken].self, from: data)
+                print("DictionaryTokens at \(filePath) read.")
+                //print(dictionaryTokens)
+            } catch {
+                print(error)
+            }
+        } else {
+            print("File \(filePath) doesn't exist")
+        }
+
+        return result
+    }
+
+    func doesKeyExistInWordsDictionary(withKey key: String) -> Bool {
+        return wordsDictionary.keys.contains(key)
+    }
+
+    func containsWordsWithPrefix(withDictionaryTokenArray dictionaryTokensArray: [DictionaryToken], withPrefix prefix: String) -> Bool {
+        let dictionaryTokensWithPrefix = dictionaryTokensArray.filter( {return $0.word.hasPrefix(prefix)} )
+        return !dictionaryTokensWithPrefix.isEmpty
+    }
+
+    func findLongestPrefixInWordsDictionary(ofWord word: String) -> String? {
+
+        if let firstLetterOfWord = word.first, firstLetterOfWord.isLetter {
+            let firstLetterOfWordsAsUppercasedString = firstLetterOfWord.uppercased()
+            if let words = wordsDictionary[firstLetterOfWordsAsUppercasedString] {
+                var endIndex = word.endIndex
+                var currentRangeOfInterest = word.startIndex..<endIndex
+                var currentPrefix = String(word[currentRangeOfInterest])
+
+                while !containsWordsWithPrefix(withDictionaryTokenArray: words, withPrefix: currentPrefix) && endIndex != word.startIndex {
+                    endIndex = word.index(before: endIndex)
+                    currentRangeOfInterest = word.startIndex..<endIndex
+                    currentPrefix = String(word[currentRangeOfInterest])
+                }
+
+                if (endIndex == word.startIndex) { return nil }
+
+                return currentPrefix
+
+            } else {
+                return nil
+                
+            }
+        } else {
+            return nil
+        }
+    }
+
+    func findClosestMatchInWordsDictionary(toInput input: String) -> DictionaryToken? {
+        
+        if let firstLetterOfInput = input.first, firstLetterOfInput.isLetter {
+            let firstLetterOfInputAsUppercasedString = firstLetterOfInput.uppercased()
+            if  firstLetterOfInputAsUppercasedString != "",
+                let words = wordsDictionary[firstLetterOfInputAsUppercasedString],
+                let longestPrefix = findLongestPrefixInWordsDictionary(ofWord: input) {
+
+                    let dictionaryTokensWithCurrentPrefix = words.filter( { return $0.word.hasPrefix(longestPrefix)} )
+
+                    if let first = dictionaryTokensWithCurrentPrefix.first { return first }
+                }
+        }
+    
+        return nil
+    }
+}
