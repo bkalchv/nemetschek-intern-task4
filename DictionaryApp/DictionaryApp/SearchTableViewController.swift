0997736672e625b80cacfc7ca8481328ec1d9927
bkalchv <bkaltchev98@gmail.com>
2022-02-14 18:09:42 +0200

    makes ExpandableTableViewCell's translationTextView unselectable and uneditable

--- DictionaryApp/DictionaryApp/SearchTableViewController.swift
+++ DictionaryApp/DictionaryApp/SearchTableViewController.swift
@@ -142,7 +142,6 @@ class SearchTableViewController: UIViewController, UISearchBarDelegate, UITableV
         if let cell = tableView.cellForRow(at: indexPath) as? ExpandableTableViewCell {
             
             if selectedCellIndexPath == indexPath {
-                //
                 selectedCellIndexPath = nil
             } else {
                 deselectPreviouslySelectedCell()
@@ -180,6 +179,7 @@ class SearchTableViewController: UIViewController, UISearchBarDelegate, UITableV
 //            }
 //        }
 //    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
